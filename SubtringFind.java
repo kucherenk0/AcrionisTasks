f7731ba8d4ac589b5930bda8214154e055ffc775
kucher <kucherenko.av@phystech.edu>
2019-06-13 01:01:39 +0300

    improved fault tolerance

--- SubtringFind.java
+++ SubtringFind.java
@@ -1,49 +1,57 @@
 import java.io.InputStreamReader;
-import java.io.IOException;
 
-public class SubtringFind{
+public class SubstringFind {
 
-    public static void main(String[] args)  {
-        if (args.length == 0) {
-            System.out.println("Input args is empty");
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("SubstringFind: Input args is empty\nusage: java SubstringFind [PATTERN]");
             System.exit(1);
         }
+        try {
+            StringBuilder builder = new StringBuilder();
+            for (String s : args) {
+                builder.append(s);
+            }
+            String needle = builder.toString();
+            int result = findAndSubstitute(needle);
+            System.exit(result);
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+            System.exit(-1);
 
-        StringBuilder builder = new StringBuilder();
-        for (String s : args) {
-            builder.append(s);
         }
-        String needle = builder.toString();
-        findAndSubstitude(needle);
+
     }
 
-    public static void findAndSubstitude(String needle) {
-        InputStreamReader in = new InputStreamReader(System.in);
-        boolean keep_going = true;
+    public static int findAndSubstitute(String needle) throws Throwable {
+        if (needle == null) {
+            return -1;
+        }
+        int len = needle.length();
+        try (InputStreamReader in = new InputStreamReader(System.in)) {
+            while (true) {
+                for (int i = 0; i < len; i++) {
+                    int charIn = in.read();
 
-        while (keep_going) {
-            for (int i = 0; i < needle.length(); ++i) {
-                int char_in;
-                try {
-                    char_in = in.read();
-                } catch (IOException e) {
-                    keep_going = false;
-                    break;
-                }
+                    if (charIn == -1) {
+                        return 0;
+                    }
 
-                if (char_in == -1) {
-                    keep_going = false;
-                    break;
-                }
+                    char nextChar = (char) (charIn);
+                    System.out.print(nextChar);
 
-                char nextChar = (char) (char_in);
-                System.out.print(nextChar);
-                if (needle.charAt(i) != nextChar) {
-                    break;
-                }
+                    if (needle.charAt(i) != nextChar) {
+                        if (nextChar == needle.charAt(0)) {
+                            i = 0;
+                            continue;
+                        } else {
+                            break;
+                        }
+                    }
 
-                if (i == needle.length() - 1) {
-                    System.out.print("*");
+                    if (i == needle.length() - 1) {
+                        System.out.print("*");
+                    }
                 }
             }
         }
